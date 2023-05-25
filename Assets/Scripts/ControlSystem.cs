bb7bcec8578885559547973940e0c4daacc3f713
gczjbd <111674762+gczjbd@users.noreply.github.com>
2023-05-25 21:21:53 +0800

    Update 0525

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -2,21 +2,34 @@
 
 public class ControlSystem : MonoBehaviour
 {
+    [Header("移動速度")]
+    [Range(0,50)]
+    public float movespeeed = 3.5f;
+    [Header("剛體")]
+    public Rigidbody2D rig;
+
     private void Awake()
     {
-        print(666);
-        print("嗨~");
-        print("1 + 2");
-        print($"{1 + 2}");
+        //print(666);
+        //print("嗨~");
+        //print("1 + 2");
+        //print($"{1 + 2}");
     }
 
     private void Start()
     {
-        print("<color=yellow>開始</color>");
+        //print("<color=yellow>開始</color>");
     }
 
     private void Update()
     {
-        print("<color=#991111>更新</color>");
+        //print("<color=#991111>更新</color>");
+        //呼叫移動方法
+        Move();
+    }
+
+    private void Move()
+    {
+        print("移動中...")；
     }
 }
